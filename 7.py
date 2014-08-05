eb6c0ab182c4a478c3b20f720e664b112a0427e0
James <james@James-imac.local>
2014-08-05 13:40:48 +0900

    Added Q7 solution

---
+++ 7.py
@@ -0,0 +1,45 @@
+from euler import *
+import time
+import math
+
+
+#start timer
+start = time.time()
+
+#question number
+questionno = 7
+
+#print the problem
+print_problem(questionno)
+
+
+#
+# Solution
+#
+
+def get_nth_prime(n):
+
+
+    primes = [2]
+    counter = 1
+    p = 3
+
+    while counter < n:
+
+        # making use of previous is_prime function to check primality
+        if is_prime(p):
+            primes.append(p)
+            counter += 1
+        
+        p+=2
+
+    return primes[n-1]
+
+print get_nth_prime(10001)
+
+print '-----------------------------------------------------------------------'
+print 'Solution:'
+print '-----------------------------------------------------------------------'
+print "Answer: %s " % 1
+print "\n"+"Execution time: %s" % (time.time()-start) 
+print '\n'
